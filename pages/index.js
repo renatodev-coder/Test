9b7a2d81c2403cd73b08ad63c863072fb0fd304b
Renato <renatodasilvamaia2@gmail.com>
2026-05-14 19:25:52 +0000

    descrição da minha jornada de aprendizado

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 function Home() {
-  return <h1>Meu treinamento inicial</h1>;
+  return (
+    <h1>
+      Meu treinamento inicial = aprendi sobre os comandos do git por exemplo:git
+      commit,status,branch,log / npm install,run, run dev, aprendi a usar o
+      editor config para deixar meu codigo traduzivel em uma unica forma para
+      outras pessoas lerem, aprendi a usar os repositorios github para poder
+      trabalhar em meus projetos de computadores diferentes apenas usando o
+      codespaces dentro do github, estou aprendendo a usar o function, if ,else,
+      usar o git ignore, aprendi a instalar versoes diferentes de um app via
+      terminal, aprendi a usar issues e milestone, aprendi a usar o
+      versionamento e ainda vou aprender mais coisas
+    </h1>
+  );
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("indentação errada");
-}
 export default Home;
